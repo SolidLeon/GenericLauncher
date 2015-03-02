294a9afee5510f95c22df561bbe26398e97ed225
SolidLeon <markusmannel@gmail.com>
2015-03-02 13:51:52 +0100

    Implementation for issue #13 - Close button label change
    Description:
    Close button now shows "Completed! Close me!"
    I want to change it to "Launch...'
    
    Resolution:
    Button is now labeled 'Launch...' as soon as the update finishes

--- src/launcher/gui/StatusDisplay.java
+++ src/launcher/gui/StatusDisplay.java
@@ -187,7 +187,7 @@ public class StatusDisplay extends JFrame implements IStatusListener, ActionList
 	 */
 	public void setStatusCompleted() {
 		closeButton.setEnabled(true);
-		closeButton.setText("Completed! Close me!");
+		closeButton.setText("Launch...");
 		overallProgress.setValue(overallProgress.getMaximum());
 		currentProgress.setMaximum(100);
 		setCurrentProgressToMax();
