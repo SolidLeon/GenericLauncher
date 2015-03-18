e22e548f9155b47dc2b62f8f8baaa9b3938d095b
SolidLeon <markusmannel@gmail.com>
2015-03-18 08:50:08 +0100

    UpdateBean and IUpdateListener check-in

---
+++ src/launcher/controller/IUpdateListener.java
@@ -0,0 +1,15 @@
+package launcher.controller;
+
+import java.util.List;
+
+import launcher.beans.UpdateBean;
+import launcher.beans.xml.XmlLauncherConfigBean;
+import launcher.beans.xml.XmlPackageBean;
+
+public interface IUpdateListener {
+
+	boolean preDownload(List<UpdateBean> toDownload);
+
+	XmlPackageBean selectPackage(XmlLauncherConfigBean remoteConfigBean);
+
+}
